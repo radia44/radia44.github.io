421b79f8e783630b8073fda47eb5b30474aa7229
radia44 <157192250+radia44@users.noreply.github.com>
2024-10-09 10:59:26 -0600

    implemented arrays and object notation

--- arrays-n-objects/sketch.js
+++ arrays-n-objects/sketch.js
@@ -5,11 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let arrows = [];
+let arrowData = [
+  ,{key: 'ArrowUp', symbol: '↑', x: 100,}
+  ,{key: 'ArrowLeft', symbol: '←', x: 150}
+  ,{key: 'ArrowDown', symbol: '↓', x: 200,}
+  ,{key: 'ArrowRight', symbol: '→', x: 250,}
+];
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(220);
-}
