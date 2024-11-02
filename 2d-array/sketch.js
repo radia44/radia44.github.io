9c483ff6d54331861a2ba119fb86756295e62146
radia44 <radiajannat44@gmail.com>
2024-11-01 22:00:26 -0600

    turned painting into a grid

--- 2d-array/sketch.js
+++ 2d-array/sketch.js
@@ -9,9 +9,11 @@
 //  [4, 5, 6]
 //  [7, 8, 9]];
 
-let grid;
 let cellSize;
-const PAINTING_GRID_SIZE = 3;
+const PAINTING_GRID_SIZE = 3; // 3x3 grid
+
+let part001, part002, part003, part004, part005, part006, part007, part008, part009;
+let pictures;
 
 function preload() {
   part001 = loadImage("image_part_001.jpg");
@@ -23,15 +25,13 @@ function preload() {
   part007 = loadImage("image_part_007.jpg");
   part008 = loadImage("image_part_008.jpg");
   part009 = loadImage("image_part_009.jpg");
+
+  pictures = [part001, part002, part003, part004, part005, part006, part007, part008, part009];
 }
-
-let pictures  = [part001, part002, part003, part004, part005, part006, part007, part008, part009];
-
 
 function setup() {
   createCanvas(668, 521);
-  cellSize = height / PAINTING_GRID_SIZE;
-  grid = generateRandomGrid(PAINTING_GRID_SIZE, PAINTING_GRID_SIZE);
+  cellSize = width / PAINTING_GRID_SIZE; // Calculate cell size based on canvas width and grid size
 }
 
 function draw() {
@@ -39,30 +39,19 @@ function draw() {
   displayGrid();
 }
 
-
-function generateRandomGrid(cols, rows) {
-  let newGrid = [];
-  for (let y = 0; y < rows; y++) {
-    newGrid.push([]);
-    for (let x = 0; x < cols; x++) {
-      //make it a 1 half the time, a 0 half the time
-      if (random(100) < 50) {
-        newGrid[y].push(1);
-      }
-      else {
-        newGrid[y].push(0);
-      }
-    }
-  }
-  return newGrid;
-}
-
 function displayGrid() {
+  let imgIndex = 0; // Track which image to display
+
   for (let y = 0; y < PAINTING_GRID_SIZE; y++) {
     for (let x = 0; x < PAINTING_GRID_SIZE; x++) {
-      fill("white");
-      stroke("black");
-      rect(x * cellSize, y * cellSize, width / PAINTING_GRID_SIZE, height / PAINTING_GRID_SIZE);
+      // Calculate the top-left corner position for each cell
+      let xPos = x * cellSize;
+      let yPos = y * cellSize;
+
+      // Display the image in the current cell
+      image(pictures[imgIndex], xPos, yPos, cellSize, cellSize);
+
+      imgIndex++; // Move to the next image in the array
     }
   }
 }
